087e88e7ec192da8ca148e14675f4af26281be8f
Sajin Thami <136429751+sajinthami@users.noreply.github.com>
2026-09-13 22:22:18 +0545

    Java program to reverse given number

--- someconcepts/src/Main.java
+++ someconcepts/src/Main.java
@@ -1,41 +1,66 @@
-import java.io.*;
+//import java.io.*;
+//
+//class geeksforgeeks {
+//
+//    // Computing the value of first fibonacci series
+//    // and storing the sum of even indexed numbers
+//    static int Fib_Even_Sum(int N)
+//    {
+//        if (N <= 0)
+//            return 0;
+//
+//        int fib[] = new int[2 * N + 1];
+//        fib[0] = 0;
+//        fib[1] = 1;
+//
+//        // Initializing the sum
+//        int s = 0;
+//
+//        // Adding remaining numbers
+//        for (int j = 2; j <= 2 * N; j++) {
+//            fib[j] = fib[j - 1] + fib[j - 2];
+//
+//            // Only considering even indexes
+//            if (j % 2 == 0)
+//                s += fib[j];
+//        }
+//
+//        return s;
+//    }
+//
+//    // The Driver code
+//    public static void main(String[] args)
+//    {
+//        int N = 11;
+//
+//        // Prints the sum of even-indexed numbers
+//        System.out.println(
+//                "Even sum of fibonacci series till number " + N
+//                        + " is: " + +Fib_Even_Sum(N));
+//    }
+//}
 
-class geeksforgeeks {
 
-    // Computing the value of first fibonacci series
-    // and storing the sum of even indexed numbers
-    static int Fib_Even_Sum(int N)
-    {
-        if (N <= 0)
-            return 0;
+// Program to reverse numbers
 
-        int fib[] = new int[2 * N + 1];
-        fib[0] = 0;
-        fib[1] = 1;
+class Main {
+    public static void main(String[] args) {
 
-        // Initializing the sum
-        int s = 0;
+        int num = 1234, reversed = 0;
 
-        // Adding remaining numbers
-        for (int j = 2; j <= 2 * N; j++) {
-            fib[j] = fib[j - 1] + fib[j - 2];
+        System.out.println("Original Number: " + num);
 
-            // Only considering even indexes
-            if (j % 2 == 0)
-                s += fib[j];
+        // run loop until num becomes 0
+        while(num != 0) {
+
+            // get last digit from num
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+
+            // remove the last digit from num
+            num /= 10;
         }
 
-        return s;
-    }
-
-    // The Driver code
-    public static void main(String[] args)
-    {
-        int N = 11;
-
-        // Prints the sum of even-indexed numbers
-        System.out.println(
-                "Even sum of fibonacci series till number " + N
-                        + " is: " + +Fib_Even_Sum(N));
+        System.out.println("Reversed Number: " + reversed);
     }
 }
