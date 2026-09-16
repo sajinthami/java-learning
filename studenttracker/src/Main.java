8d8136d6ba169110dc2b12abc9da29bc6509dded
Sajin Thami <136429751+sajinthami@users.noreply.github.com>
2026-09-16 21:39:27 +0545

    Changed the folder

--- studenttracker/src/Main.java
+++ studenttracker/src/Main.java
@@ -1,33 +1,3 @@
-//import java.io.File;
-//import java.io.FileWriter;
-//import java.io.IOException;
-//import java.util.Scanner;
-//
-//public class Main {
-//    public static void main (String[] args) {
-//
-//
-//        try {
-//            File file = new File ("learningjava.txt");
-//            FileWriter writer = new FileWriter("learningjava.txt");
-//            writer.write("Hello I am learning java file handling.");
-//            writer.close();
-//
-//            Scanner scanner = new Scanner("learningjava.txt");
-//
-//            while (scanner.hasNextLine()) {
-//                String line = scanner.nextLine();
-//                System.out.println(line);
-//            }
-//
-//            scanner.close();
-//
-//        } catch (IOException e) {
-//            System.out.println("Something went wrong");
-//        }
-//    }
-//
-//}
 
 import java.io.File;
 import java.io.FileWriter;
