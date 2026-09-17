c70251ee43cc48a5d3c675964edf97835c8221d1
Sajin Thami <136429751+sajinthami@users.noreply.github.com>
2026-09-17 20:56:43 +0545

    Learning about OOP

---
+++ lerningOOP/src/Car.java
@@ -0,0 +1,15 @@
+public class Car {
+    String make = "Ford";
+    String model = "Mustang";
+    int year = 2023;
+    double price = 56999.99;
+    boolean isRunning = false;
+
+    void start() {
+        System.out.println("You start the engine.");
+    }
+
+    void stop() {
+        System.out.println("You stop the engine.");
+    }
+}
---
+++ lerningOOP/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        // Object = An entry that holds data (attributes)
+        //          and can perform actions (methods)
+        //          It is a reference data type
+
+        Car car = new Car();
+
+        System.out.println(car.make + "  " + car.model);
+
+    }
+}
